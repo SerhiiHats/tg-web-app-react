d4b8b42a311ba85ef995f673eb2eebf691f52023
Serhi Hats <gatsserv@gmail.com>
2024-05-29 01:26:30 +0300

    fix card product

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,11 +2,27 @@ import "./ProductList.css";
 import React from 'react';
 import ProductItem from "../ProductItem/ProductItem";
 
-const ProductList = ({products}) => {
+
+
+const ProductList = () => {
+  const products = [
+    {id: "1", title: "Джинси", price: 1200, description: "Блакитного кольору"},
+    {id: "2", title: "Джинси", price: 1200, description: "Блакитного кольору"},
+    {id: "3", title: "Джинси", price: 1200, description: "Блакитного кольору"},
+    {id: "4", title: "Джинси", price: 1200, description: "Блакитного кольору"},
+    {id: "5", title: "Джинси", price: 1200, description: "Блакитного кольору"},
+    {id: "6", title: "Джинси", price: 1200, description: "Блакитного кольору"},
+    {id: "7", title: "Джинси", price: 1200, description: "Блакитного кольору"},
+    {id: "8", title: "Джинси", price: 1200, description: "Блакитного кольору"},
+  ]
+  const onAdd = ()=>{}
   return (
-    <div>
+    <div className={"list"}>
       {products.map(item=>(
-        <ProductItem product={item.product} onAdd={item.onAdd}/>
+        <ProductItem className={"item"} key={item.id}
+                     product={item}
+                     className={""}
+                     onAdd={onAdd}/>
       ))}
     </div>
   );
